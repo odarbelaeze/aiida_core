8096eb9bd6d9504478468e31270b5f58fc35b968
Oscar Arbeláez <odarbelaeze@gmail.com>
2019-03-26 12:55:17 +0100

    Prepare the release in a way

--- docs/source/working/include/snippets/processes/functions/signature_plain_python_call_default.py
+++ docs/source/working/include/snippets/processes/functions/signature_plain_python_call_default.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###########################################################################
+# Copyright (c), The AiiDA team. All rights reserved.                     #
+# This file is part of the AiiDA code.                                    #
+#                                                                         #
+# The code is hosted on GitHub at https://github.com/aiidateam/aiida_core #
+# For further information on the license, see the LICENSE.txt file        #
+# For further information please visit http://www.aiida.net               #
+###########################################################################
 
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 def add_multiply(x, y, z=1):
     return (x + y) * z
 
--- docs/source/working/include/snippets/workflows/expose_inputs/run_simple.py
+++ docs/source/working/include/snippets/workflows/expose_inputs/run_simple.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env runaiida
 # -*- coding: utf-8 -*-
+###########################################################################
+# Copyright (c), The AiiDA team. All rights reserved.                     #
+# This file is part of the AiiDA code.                                    #
+#                                                                         #
+# The code is hosted on GitHub at https://github.com/aiidateam/aiida_core #
+# For further information on the license, see the LICENSE.txt file        #
+# For further information please visit http://www.aiida.net               #
+###########################################################################
+from __future__ import division
+from __future__ import absolute_import
 from __future__ import print_function
 
 from aiida.orm import Bool, Float, Int
